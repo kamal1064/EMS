85be5ed63b4eb35d5991049d099f4d7c8633e6da
kamal1064 <127846792+kamal1064@users.noreply.github.com>
2026-06-26 00:06:15 -0700

    fix: resolve BuildError by correcting url_for endpoint from part_time_employee to part_time

--- app.py
+++ app.py
@@ -1905,11 +1905,11 @@ def delete_part_time_worker(worker_id):
     invalidate_dashboard_cache(uid)
     worker_id_obj = safe_object_id(worker_id)
     if not worker_id_obj:
-        return redirect(url_for('part_time_employee'))
+        return redirect(url_for('part_time'))
         
     worker = db.part_time_workers.find_one({"_id": worker_id_obj, "user_id": ObjectId(uid)})
     if not worker:
-        return redirect(url_for('part_time_employee'))
+        return redirect(url_for('part_time'))
         
     # Get all work logs to delete their advances
     work_logs = list(db.part_time_work_logs.find({"worker_id": worker_id_obj}))
@@ -1921,7 +1921,7 @@ def delete_part_time_worker(worker_id):
     db.part_time_work_logs.delete_many({"worker_id": worker_id_obj})
     db.part_time_workers.delete_one({"_id": worker_id_obj})
     
-    return redirect(url_for('part_time_employee'))
+    return redirect(url_for('part_time'))
 
 @app.route('/salary/mark_paid', methods=['POST'])
 @limiter.limit("30 per minute")         # prevent accidental bulk payment triggers
